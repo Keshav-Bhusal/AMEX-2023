06d2d5d930848d320af14efb6754f2c7ffe40056
Keshav Bhusal <113562860+Keshav-Bhusal@users.noreply.github.com>
2023-11-22 00:37:31 -0700

    Add files via upload

--- CODES/modelling.R
+++ CODES/modelling.R
@@ -1,43 +1,10 @@
 ##########################################################################
-#PS4
-#DATE:12 Nov, 2023
+#PS12
+#DATE:20 Nov, 2023
 #Author: Keshav Bhusal
 #AREC 559 Advanced Applied Econometric 
 #Affiliation: Department of Agriculture and Resource Economics(AREC)
 ##########################################################################
-
-
-#Variables of Interest 
-
-count_variables <- c(count_kept_promise,count_brok_promise, count_phone_rpc,                    
-                     count_phone_inbound, count_phone_outbound , count_phone_outbound_attempts,      
-                     count_chat, count_sms,  count_email,                        
-                     count_email_received,count_letter_sent,count_letter_received   ,           
-                     count_email_letter_sent, count_kept_promise_before,count_kept_promise_after,           
-                     count_brok_promise_before,count_brok_promise_after ,count_phone_rpc_before,             
-                     count_phone_rpc_after,count_phone_inbound_before ,count_phone_inbound_after,         
-                     count_phone_outbound_before ,count_phone_outbound_after,count_phone_outbound_attempt_before,
-                     count_phone_outbound_attempt_after,count_chat_before , count_chat_after,                  
-                     count_sms_before ,count_sms_after , count_email_before ,                
-                     count_email_after ,count_email_received_before ,count_email_received_after,       
-                     count_letter_sent_before , count_letter_sent_after, count_letter_received_before ,      
-                     count_letter_received_after, count_email_letter_sent_before, count_email_letter_sent_after)
-                     
-                  
-normalized_variables <- c(nrm_mob_login_count ,nrm_mob_login_count_before,nrm_mob_login_count_after,          
-                           nrm_web_login_count,nrm_web_login_count_before, nrm_web_login_count_after ,         
-                           nrm_both_login_count, nrm_both_login_count_before , nrm_both_login_count_after ,        
-                           nrm_phone_rpc  , nrm_phone_rpc_before ,nrm_phone_rpc_after,                
-                           nrm_kept_promise , nrm_kept_promise_before, nrm_kept_promise_after ,            
-                           nrm_brok_promise , nrm_brok_promise_before , nrm_brok_promise_after ,            
-                           nrm_phone_inbound , nrm_phone_inbound_before , nrm_phone_inbound_after,
-                           nrm_phone_outbound , nrm_phone_outbound_before , nrm_phone_outbound_after ,          
-                           nrm_phone_outbound_attempts , nrm_phone_outbound_attempts_before , nrm_phone_outbound_attempts_after,  
-                           nrm_chat, nrm_chat_before , nrm_chat_after,                  
-                           nrm_sms ,nrm_sms_before , nrm_sms_after,                      
-                           nrm_email_sent , nrm_email_sent_before , nrm_email_sent_after,               
-                           nrm_email_received , nrm_letter_sent , nrm_letter_sent_before,             
-                           nrm_letter_sent_after ,nrm_letter_received  , nrm_email_letter)
 
 #Setting up the environment
 library(rio)
@@ -126,6 +93,9 @@ early_legal$exposure_bins <- factor(early_legal$exposure_bins,
 early_legal$cm_type <- factor(early_legal$cm_type, 
                                       levels = c("Both", "Business_Only", "Customer_Only"))
 
+# Exporting the final data file
+export(early_legal, "working_data/updated_version.RData")
+
 #Checking the summary of the categories: exposure_bins 
 check <- early_legal %>% 
   group_by(exposure_bins) %>% 
@@ -151,27 +121,8 @@ check2 <- early_legal %>%
 check2
 
 ##########################################################################
-#Prediction: Logistic Model (General)
+# Trying different Models 
 ##########################################################################
-
-model1 <- glm(pvcDUM ~  del_FICO_score_norm + fico_categories +
-              + co_exposure_to_tca_due + exposure_to_tca_due + exposure_bins
-              + age_co_to_earlylegal + age_el_to_caseclose + 
-              + phone_rpc + phone_inbound +  phone_outbound + brok_promise + chat + sms + email_sent + email_letter_sent + mobile + web + both
-              + cm_type, data = early_legal, family = binomial(link = "logit"))
-
-summary(model1)
-margins(model1)
-
-logit_margins <- summary(margins(model1))
-
-stargazer(model1, logit_margins, type="text",
-          dep.var.labels="Sent to Collection",
-          covariate.labels=c("FICO Score", "Total Exposure", "Total Case Amount Due", "Total Past Due", "Exposure to TCA Due",
-                             "Case Open to EL (Days)", "EL to Case Close (Days)", "Phone Contact", "Phone Inbound",  "Phone Outbound",
-                             "Chat", "Email Sent", "letter Sent", "Email letter Sent", "Mobile Only", "Web Only",  "Both",  "Card Member Type"),
-          out="msg_models.txt")
-
 ##########################################################################
 #Prediction: Logistic Model (General Dummy Model)
 ##########################################################################
